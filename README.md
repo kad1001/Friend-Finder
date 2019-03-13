3d75297117205008aaa05e49519ffec047b7c1c0
Kelly davis <kellyadavis7@gmail.com>
2019-03-13 02:02:50 -0400

    button changes

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Friend-Finder
 Full-stack site that takes in results from users' surveys, then compares their answers with those from other users.
 
-<h2>Technologies Used</h2>
+<h2>Technologies Used:</h2>
 <ul>
+<li>Express.js</li>
 <li>Node.js</li>
 <li>Javascript</li>
+<li>HTML/CSS<li>
+<li>Bootstrap</li>
 </ul>
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// app.use(express.static(__dirname + '/public'));
+
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
