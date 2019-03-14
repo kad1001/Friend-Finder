2cb15ade60b787d6af0cf6f3109e88261e784af3
Kelly davis <kellyadavis7@gmail.com>
2019-03-14 01:20:28 -0400

    this shit is bananas

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,9 +6,9 @@
 var friendsData = [{
     name: "Santa Claus",
     photo: "https://r.hswstatic.com/w_907/gif/santa-claus-orig.jpg",
-    // photoupload: "hi",
     scores: ["5", "2", "5", "1"]
 }];
+
 
 // makes it accessible to other files using require.
 module.exports = friendsData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,21 +7,108 @@ module.exports = function (app) {
   // Below code handles when users "visit" a page.
 
   // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendsData);
   });
+
 
   // ---------------------------------------------------------------------------
   // A POST routes /api/friends. This will be used to handle incoming survey results. 
   // This route will also be used to handle the compatibility logic.
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
     friendsData.push(req.body);
-    for (let i = 0; i < friendsData.length; i++) {
-      var s = friendsData[i].scores;
-      console.log(s);
+
+
+
+    function compare(arr1, arr2) {
+      if (!arr1 || !arr2) return
+
+      let result;
+      arr1.forEach((e1, i) => arr2.forEach(e2 => {
+
+        if (e1.length > 1 && e2.length) {
+          result = compare(e1, e2);
+        } else if (e1 !== e2) {
+          result = false
+        } else {
+          result = true
+        }
+      }))
+
+      return result
 
     }
-    // return s
-  })
 
+
+
+    function difference(a, b) {
+      console.log(a)
+      console.log(parseInt(a))
+
+      //  console.log(Math.abs(a - b))
+      return Math.abs(a - b);
+    }
+
+    function getScores(item) {
+      // puts the /api/friends body contents into an array for comparison
+      var scores = item.scores;
+      for (i = 0; i < scores.length; i++) {
+        scores[i] = parseInt(scores[i])
+      }
+      return scores;
+    }
+
+    function mappin() {
+      // finds the scores array in the friendsdata object
+      var mapped = friendsData.map(getScores);
+      console.log(mapped);
+
+      console.log(compare(mapped[0], mapped[1]));
+      // console.log()
+
+
+      // all scores for q1
+      // var positionOne = [];
+      // var positionTwo = [];
+      // for (i = 0; i < mapped.length; i++) {
+      // var q1score = mapped[i].slice(0,1);
+      // // console.log(q1score.join())
+      // var q2score = mapped[i].slice(1,2);
+
+      // positionOne.push(q1score);
+      // positionTwo.push(q2score);
+      // }
+
+      // console.log("question 1 answers", positionOne[0]);
+
+      // console.log("question 2 answers", positionTwo);
+
+      // var r = positionOne.reduce(getSum);
+      // console.log(r);
+
+      return mapped
+    }
+
+    // by running this function the combined array is console logged
+    mappin();
+
+
+
+
+
+    //  console.log(compare([1,2,3],[1,2,3]));
+
+
+    // var result = friendsData.map(function (x) {
+    //     return parseInt(x);
+    // });
+    // console.log(result);
+
+
+    // var result = friendsData.map(function (x) {
+    //   return parseInt(x);
+    //   console.log(parseInt(x));
+    // });
+    // console.log(result);
+  })
 };
