3e9888ff98304ca0b1f979a514614c9a06c11142
Kelly davis <kellyadavis7@gmail.com>
2019-03-23 21:22:46 -0400

    cleaned up

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,11 @@
 // Friend compatability logic 
 
-
-
-// Example friends
+// Get example friends
 const friendsData = require("../data/friends");
-// Friend Object Constructor
-const Friend = require("./logic");
 
+// In order for the user to have the same object methods as exisiting friends --
+const Friend = require('./logic')
+// (random global function)
 // Must be used with .reduce method
 function getSum(total, num) {
   return total + num;
@@ -17,99 +16,91 @@ function getSum(total, num) {
 module.exports = function (app) {
   // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
   app.get("/api/friends", function (req, res) {
-    // appends each of the friends data to the friends
+
     res.json(friendsData);
+
   });
 
-
   // ---------------------------------------------------------------------------
-  // A POST routes /api/friends. This will be used to handle incoming survey results. 
-  // This route will also be used to handle the compatibility logic.
+  // A POST routes /api/friends
   app.post("/api/friends", function (req, res) {
 
-    // User input
+    // Capture user input
     let current = req.body;
-    console.log(friendsData)
-    const userFriend = new Friend(current.name, current.photo, current.scores);
 
 
-    let parsed = userFriend.scores.map(function(x){
+    let cScores = current.scores;
+
+    // Parse the current user's scores - return an array
+    let parsed = cScores.map(function (x) {
       return parseInt(x);
     })
 
+
     for (let c = 0; c < friendsData.length; c++) {
-      
       // Parses the scores for each example friend
       friendsData[c].newd.push(friendsData[c].scores.map(function (x) {
         return parseInt(x);
-       })
-       )
-
+      }))
     }
-    console.log(friendsData)
 
-    friendsData.forEach(function(example) {
-      // console.log(example.newd[0]);
+
+    friendsData.forEach(function (example) {
+
+      // Feeds the parsed scores into an object method that returns each scores differences
       var t = example.newArray(parsed);
-      // console.log("differences:", t);
 
+      // Get the sum of this
       var f = t.reduce(getSum);
-      // console.log("Sum of differences: ", f);
 
-      // example.bingbong(f);
+      // Save in Friend object
       return example.total = f;
-
     })
 
 
+    // Holds the added scores in an array
     var scorese = [];
 
-    // If the total differences of scores is less than 10
-    for (let g = 0; g < friendsData.length; g++){
-      // if (friendsData[g].total < 10){
-      //   friendName.push(friendsData[g].name);
-      //   friendImage.push(friendsData[g].photo);
-      // }
+    // Matches the lowest total out of the array of friend objects
+    for (let g = 0; g < friendsData.length; g++) {
 
       var minimum = Math.min(friendsData[g].total);
       console.log(minimum);
 
       scorese.push(minimum);
-      // if (minimum) {
-      // scorea.push(({score: minimum, friendName: friendsData[g].name, friendImage: friendsData[g].photo}));
-      // }
-      // scorea.push({totalscore: minimum, friend: friendsData[g]})
+
     }
-// console.log(scorea)
-console.log(scorese);
 
+    // get the minimum out of this array
     var thismin = Math.min(...scorese);
-    console.log(thismin)
-    // console.log(friendsData)
+    console.log("lowest total difference of scores:", thismin)
 
+
+    // This is the array that gets sent to front end
     var resr = [];
-    // console
-    // const result = friendsData.total.filter(word => word.length > 6);
-    // console.log(result)
 
-    friendsData.forEach(function(friend){
-      console.log(friend);
-      console.log(friend.total)
+    friendsData.forEach(function (friend) {
+
+      // See if the friend's total is the lowest
+
       if (friend.total === thismin) {
 
-        resr.push({friendName: friend.name, friendImage: friend.photo});
+        // Globalize winner winner chicken dinner friend
+        resr.push({
+          friendName: friend.name,
+          friendImage: friend.photo
+        });
       }
+
     })
-    console.log(resr)
-    // var e = friendName.pop();
-    // var i = friendImage.pop();
-    // console.log(i)
 
-		// Add new user
-    // friendsData.push(current);
+    console.log("closest match:", resr)
 
-		// Send response to frontend in an object
+    // Add new user
+    friendsData.push(new Friend (current.name, current.photo, cScores));
+
+    // Send response to frontend -- if there are more than one match then it will only display the first one
     res.json(resr[0]);
 
-    });
+  });
 }
